ee7a66bf55919536565bd7dbb5ab26d0e984ba91
aimal-khann <amailkhan313@gmail.com>
2025-12-18 00:30:14 +0500

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3001/api/auth",
+  baseURL: "https://betterauth-backend.up.railway.app/api/auth",
 });
